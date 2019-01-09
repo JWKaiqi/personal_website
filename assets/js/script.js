23b23c2afb84d1c967ef89ee6d95cc2c40b99679
Jennifer <k43wu@uwaterloo.ca>
2019-01-08 22:01:01 -0500

    init commit

---
+++ assets/js/script.js
@@ -0,0 +1,71 @@
+///////////////////////////////
+// One page Smooth Scrolling
+///////////////////////////////
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
+
+
+
+$(document).ready(function() {
+
+    // static navigationbar
+    var changeStyle = $('#navigation-bar');
+
+    function scroll() {
+        if ($(window).scrollTop() > 640) {
+            changeStyle.addClass('navbar-fixed-top');
+            changeStyle.addClass('one-page-nav');
+        } else {
+            changeStyle.removeClass('navbar-fixed-top');
+            changeStyle.removeClass('one-page-nav');
+        }
+    }
+
+    document.onscroll = scroll;
+
+    $('.testimonial-owl').owlCarousel({
+        items: 1
+    });
+
+    $('.add-owl').owlCarousel({
+        items: 4,
+        nav: true,
+        navText: false,
+        dots: false,
+        loop: true
+    });
+
+    $('.twitter-owl').owlCarousel({
+        items: 1,
+        nav: true,
+        navText: false,
+        dots: false,
+        loop: true
+    });
+
+});
+
+function viewMoreProject() {
+    var showMoreDiv =  document.getElementById("showMore");
+    var projectBtn = document.getElementById("project-btn");
+    if(showMoreDiv.style.opacity == 0) {
+        showMoreDiv.style.opacity = 1;
+        showMoreDiv.style.display = "block";
+        projectBtn.innerHTML ="HIDE";
+    } else {
+        showMoreDiv.style.opacity = 0;
+        showMoreDiv.style.display = "none";
+        projectBtn.innerHTML ="VIEW MORE";
+        document.location.href = "#project";
+    }
+}
